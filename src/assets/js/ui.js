9a1d66d1bc5f97ea0b0c6bb15b2ab3ae8b172201
alexbcberio <alexbcberio@gmail.com>
2017-05-25 22:53:50 +0200

    Added small guide for first run

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -7,12 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }, false);
     });
 
-    document.querySelector("#titleBar img").addEventListener("click", () => {
-        if (ipcRenderer.send("titleBar", "loadFolder")) {
-            while (document.querySelectorAll("#songs [data-type='audio']").length > 0) {
-                document.querySelector("#songs [data-type='audio']").remove();
+    document.querySelectorAll("#titleBar img, #songs [data-action='openDir']").forEach((element) => {
+        element.addEventListener("click", () => {
+            if (ipcRenderer.send("titleBar", "loadFolder")) {
+                while (document.querySelectorAll("#songs [data-type='audio']").length > 0) {
+                    document.querySelector("#songs [data-type='audio']").remove();
+                }
             }
-        }
+        });
     });
 
 }, false);
