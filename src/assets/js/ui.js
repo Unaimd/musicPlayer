c8fbc5d3ec3f06ca1950a3a2da153f395dd60470
alexbcberio <alexbcberio@gmail.com>
2017-07-09 16:16:44 +0200

    Selecting a new folder was not working die to changes done o previous commits

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -14,7 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll("#titleBar img, #songs [data-action='openDir']").forEach((element) => {
         element.addEventListener("click", () => {
-            if (ipcRenderer.send("titleBar", "loadFolder")) {
+            if (ipcRenderer.send("titleBar", {
+                win: element.parentNode.getAttribute("data-win"),
+                action: "loadFolder"
+            }) == true) {
                 while (document.querySelectorAll("#songs [data-type='audio']").length > 0) {
                     document.querySelector("#songs [data-type='audio']").remove();
                 }
