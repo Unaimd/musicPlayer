cb4245a90d2b6cf504a514194ddabdddec5be6ad
alexbcberio <alexbcberio@gmail.com>
2017-07-11 23:32:55 +0200

    Added fs module load

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,21 @@
 const {ipcRenderer} = require("electron");
+const {shell} = require("electron");
+const fs = require("fs");
 
-if (typeof Hotkey !== "function") {
-    throw new Error("Hotkey class required");
-} else {
+document.addEventListener("DOMContentLoaded", () => {
 
-    new Hotkey("ctrl + shift + I", () => {
-        ipcRenderer.send("toggleDevTools");
-    });
+    if (typeof Hotkey !== "function") {
+        throw new Error("Hotkey class required");
+    } else {
 
-    new Hotkey("ctrl + O", () => {
-        ipcRenderer.send("showOptions");
-    });
+        new Hotkey("ctrl + shift + I", () => {
+            ipcRenderer.send("toggleDevTools");
+        });
 
-}
+        new Hotkey("ctrl + O", () => {
+            ipcRenderer.send("showOptions");
+        });
+
+    }
+
+}, false);
